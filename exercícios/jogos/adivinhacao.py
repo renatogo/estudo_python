93b172d9319e171866f60834bf62d094ca317a5c
Renato G. Oliveira <rg.oliveira@hotmail.com>
2021-06-28 20:03:29 -0300

    implementação da classe forca

--- exercícios/jogos/adivinhacao.py
+++ exercícios/jogos/adivinhacao.py
@@ -1,35 +1,37 @@
-print("********************************")
-print("Bem vindo ao jogo de adivinhação")
-print("********************************")
+def jogar_adicinhacao():
 
-# declaração das variáveis
-numero_secreto = 42
-numero_tentativas = 3
-total_tentativas = numero_tentativas
-chute = 0
-rodada = 1
+    print("********************************")
+    print("Bem vindo ao jogo de adivinhação")
+    print("********************************")
 
-# testes cíclicos
-while (numero_tentativas > 0):
-    print("Tentativa {} de {}:".format(rodada, total_tentativas))
-    # recupera o valor digitado na tela
-    chute = input("Digite o seu número: ")
-    print("Você digitou: ", chute)
+    # declaração das variáveis
+    numero_secreto = 42
+    numero_tentativas = 3
+    total_tentativas = numero_tentativas
+    chute = 0
+    rodada = 1
 
-    # declaração de variáveis associadas a operações
-    acertou = numero_secreto == int(chute)
-    maior = int(chute) > numero_secreto
-    menor = int(chute) < numero_secreto
+    # testes cíclicos
+    while (numero_tentativas > 0):
+        print("Tentativa {} de {}:".format(rodada, total_tentativas))
+        # recupera o valor digitado na tela
+        chute = input("Digite o seu número: ")
+        print("Você digitou: ", chute)
 
-    if (acertou):
-        print("Você acertou")
-    else:
-        if(maior):
-            print("Você errou! O seu chute foi maior do que o número correto")
-        elif(menor):
-            print("Você errou! O seu chute foi menor do que o número secreto")
+        # declaração de variáveis associadas a operações
+        acertou = numero_secreto == int(chute)
+        maior = int(chute) > numero_secreto
+        menor = int(chute) < numero_secreto
 
-    numero_tentativas = numero_tentativas - 1
-    rodada = rodada + 1
+        if (acertou):
+            print("Você acertou")
+        else:
+            if(maior):
+                print("Você errou! O seu chute foi maior do que o número correto")
+            elif(menor):
+                print("Você errou! O seu chute foi menor do que o número secreto")
 
-print("Fim do jogo Renato!")
+        numero_tentativas = numero_tentativas - 1
+        rodada = rodada + 1
+
+    print("Fim do jogo Renato!")
--- exercícios/jogos/forca.py
+++ exercícios/jogos/forca.py
@@ -3,8 +3,17 @@ def jogar_forca():
     print("****Bem vindo ao Joga da Forca****")
     print("**********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    # enquanto não enforcou e não acertou
+    while not enforcou and not acertou:
+        print("jogando ...")
+
     print("Fim do Jogo")
 
 # identifica se a classe está sendo chamada de outra classe (import) ou se está sendo chamada diretamente
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     jogar_forca()
