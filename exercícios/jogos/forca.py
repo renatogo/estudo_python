e0f45daa7e3c26a180ceaaac90506ca942ae064b
renato <rg.oliveira@hotmail.com>
2021-06-29 20:32:37 -0300

    formatações de string em forca.py

--- exercícios/jogos/forca.py
+++ exercícios/jogos/forca.py
@@ -10,7 +10,14 @@ def jogar_forca():
 
     # enquanto não enforcou e não acertou
     while not enforcou and not acertou:
-        print("jogando ...")
+        chute = input("Qual letra? ")
+        chute = chute.strip() # retira os espaços em branco do início e fim da string
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.lower() == letra.lower()):
+                print("Encontrado a letra {} no indiex {}".format(chute, index))
+            index = index + 1
 
     print("Fim do Jogo")
 
