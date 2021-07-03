fd5ada5d7819d056aa84e6413382477c42bdb798
renato <rg.oliveira@hotmail.com>
2021-07-03 18:54:54 -0300

    operação com string

--- exercícios/jogos/forca.py
+++ exercícios/jogos/forca.py
@@ -4,21 +4,23 @@ def jogar_forca():
     print("**********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
 
     # enquanto não enforcou e não acertou
     while not enforcou and not acertou:
+        print(letras_acertadas)
         chute = input("Qual letra? ")
-        chute = chute.strip() # retira os espaços em branco do início e fim da string
+        chute = chute.strip()  # retira os espaços em branco do início e fim da string
 
         index = 0
         for letra in palavra_secreta:
-            if(chute.lower() == letra.lower()):
-                print("Encontrado a letra {} no indiex {}".format(chute, index))
+            if chute.lower() == letra.lower():
+                letras_acertadas[index] = letra
+                #  print("Encontrado a letra {} no indiex {}".format(chute, index))
             index = index + 1
-
     print("Fim do Jogo")
 
 # identifica se a classe está sendo chamada de outra classe (import) ou se está sendo chamada diretamente
