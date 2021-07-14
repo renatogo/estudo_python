73bf715d5deda523690dff3fd39d123f0b00d735
renato <rg.oliveira@hotmail.com>
2021-07-14 20:58:45 -0300

    fatiamento de strings

---
+++ exercícios/extrator-url/main.py
@@ -0,0 +1,17 @@
+url="https://bytebank.com/cambio?moedaOrigem=real&moedaDEstino=dolar&quantidade=100"
+print(url)
+
+url_base=url[0:27]
+print(url_base)
+
+url_parametro_moeda_origem=url[28:44]
+print(url_parametro_moeda_origem)
+
+# utilizando o método find
+indice_interrogacao = url.find("?")
+
+url_base=url[0:indice_interrogacao]
+print(url_base)
+
+url_parametro_moeda_origem=url[indice_interrogacao+1:44]
+print(url_parametro_moeda_origem)
